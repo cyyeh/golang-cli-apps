937d365c8e8fac7bcb95c84ab3faf4b41d965688
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2022-06-11 19:08:46 +0800

    add flag

--- word-counter/main.go
+++ word-counter/main.go
@@ -4,17 +4,21 @@ package main
 
 import (
 	"bufio" // To read text
+	"flag"  // Create and manage command-line flags
 	"fmt"   // To print formatted output
 	"io"    // Provides the io.Reader interface
 	"os"    // Use os resources
 )
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
 	// A scanner is used to read text from a Reader(such as files)
 	scanner := bufio.NewScanner(r)
 
-	// Define the scanner split type to words(default is split by lines)
-	scanner.Split(bufio.ScanWords)
+	// If the count lines flag is not set, we want to count words so we define
+	// the scanner split type to words(default is split by lines)
+	if !countLines {
+		scanner.Split(bufio.ScanWords)
+	}
 
 	// Defining a counter
 	wc := 0
@@ -29,7 +33,11 @@ func count(r io.Reader) int {
 }
 
 func main() {
-	// Calling the count function to count the number of words
+	// Defining a boolean flag -l to count lines instead of words
+	lines := flag.Bool("l", false, "Count lines")
+	// Parsing the flags provided by the user
+	flag.Parse()
+	// Calling the count function to count the number of words (or lines)
 	// received from the Standard Input and printing out
-	fmt.Println(count(os.Stdin))
+	fmt.Println(count(os.Stdin, *lines))
 }
